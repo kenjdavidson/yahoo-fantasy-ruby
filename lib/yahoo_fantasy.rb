58f63df81ac7accdaa395215407469c0249102d5
kdavidson <kdavidson@standardbredcanada.ca>
2021-10-29 22:43:16 +0000

    (fix): devcontainers and rubocop formatting

---
+++ lib/yahoo_fantasy.rb
@@ -0,0 +1,6 @@
+require "yahoo_fantasy/version"
+
+module YahooFantasy
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/yahoo_fantasy/version.rb
@@ -0,0 +1,3 @@
+module YahooFantasy
+  VERSION = "0.1.0"
+end
